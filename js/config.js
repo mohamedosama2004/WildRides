bb0e1ddb82db38ba0062bd12bdad9b9b0d63d4be
Mohamed Osama <146884487+mohamedosama2004@users.noreply.github.com>
2026-04-26 18:56:07 +0300

    Fix formatting in config.js for consistency

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-central-1_3ZEZo1Xv9', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2q7jcavechbvfb5mmk0hcq9apk ', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '2q7jcavechbvfb5mmk0hcq9apk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
