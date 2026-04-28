cbdabb331e4c36bc6ba3eeef7a56213df69b60d0
Mohamed Osama <146884487+mohamedosama2004@users.noreply.github.com>
2026-04-29 01:34:21 +0300

    Updated js/config.js with new configuration on 2026-04-28 22:34:12 UTC

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://ing1evpc88.execute-api.eu-central-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://ing1evpc88.execute-api.eu-central-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod
     }
 };
